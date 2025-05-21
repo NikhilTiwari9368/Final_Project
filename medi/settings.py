7d4af959e89ffdb57557cbc880139f4bffef68e9
Nikhil <nikhil.tiwari@digiprima.co>
2025-05-21 15:46:35 +0530

    Update the Database in the project

--- medi/settings.py
+++ medi/settings.py
@@ -84,9 +84,16 @@ WSGI_APPLICATION = "medi.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
